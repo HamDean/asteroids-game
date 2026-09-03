70eff392f73fd06ed9422728290065fc388b6ac9
HamDean <hamdeen247@gmail.com>
2026-09-03 21:20:41 +0000

    Draw asteroids to screen

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from pygame.mixer_music import play
-
+from asteroid import Asteroid
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 import pygame
 from player import Player
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -14,12 +15,15 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
 
-
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = [updatable]
 
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
 
     while True:
