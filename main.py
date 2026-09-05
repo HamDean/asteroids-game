f4fe4597415aacd2fd25cf9c5b39192fde813429
HamDean <hamdeen247@gmail.com>
2026-09-05 14:22:00 +0000

    Implement shooting logic

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pygame
 from player import Player
 from asteroidfield import AsteroidField
 import sys
+from shot import Shot
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -19,10 +20,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = [updatable]
+    Shot.containers = (shots, drawable, updatable) 
 
     asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
